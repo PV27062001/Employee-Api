bb5e2cdcf9c4a11b3f548727d540af4545c0f0e3
praveen <100416844+PV27062001@users.noreply.github.com>
2025-11-08 15:33:59 +0530

    changed the random key generator to static one

--- src/main/java/com/employeeApplication/interview/InterviewController.java
+++ src/main/java/com/employeeApplication/interview/InterviewController.java
@@ -10,6 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/interview")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class InterviewController {
     private final InterviewService interviewService;
 
